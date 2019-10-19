fa8eda88248501ff44c4d8439d4736f6f68b91d2
Fabian Windheuser <windheuser.fabian@gmail.com>
2019-10-19 14:01:37 +0200

    Rename app (#19)
    * Rename application
    * Update readme

--- examples/notes-service/record.sh
+++ examples/notes-service/record.sh
@@ -3,7 +3,7 @@ set -e
 
 (cd ../.. && make)
 
-../../docker-test record \
+../../looper record \
     --service notes-service \
     --compose ./docker-compose.yml \
     --test 'cd ./tests && npm test'
